3a7a61d3aa5e552f1df5ade3ec107ac1458e24b2
joe <714070832@qq.com>
2020-08-22 22:26:15 +0800

    login_firstdev

---
+++ src/components/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="login_container">
+    登录组件
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less" scoped>
+#login_container{
+    background-color: #2b5b6b;
+    height: 100%;
+}
+</style>
